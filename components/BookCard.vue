e79f77ea2fa33ee53fb00de54baa35bfcdc10200
Muhammad Inam Ur Rehman <inaamurrehman343@gmail.com>
2024-01-24 22:00:10 +0500

    Added public imagess

---
+++ components/BookCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex flex-col gap-4 text-center text-white">
+    <NuxtImg :src="image" alt="SHIFTMAKERS BOOK 1" />
+    <h2 class="!font-vp600 max-w-[180px] mx-auto text-center">{{ title }}</h2>
+    <BtnWithLogo :btn-text="btnLabel" :href="btnLink" class="mx-auto" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  image: string;
+  title: string;
+  btnLabel: string;
+  btnLink: string;
+}>();
+</script>
+
+<style></style>
---
+++ pages/boek.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <Bounded class="grid gap-6 my-16 grid-cols1 lg:grid-cols-2">
+      <div class="grid grid-cols-1 gap-12 p-4 bg-theme-blue sm:grid-cols-2">
+        <BookCard
+          image="/images/shiftmakers.webp"
+          title="Shiftmakers. L’art du (self)leaderschap dans les années 2020."
+          btnLabel="BESTEL NU"
+          btnLink="/"
+        />
+        <BookCard
+          image="/images/shiftmakers2.webp"
+          title="Shiftmakers. L’art du (self)leaderschap dans les années 2020."
+          btnLabel="BESTEL NU"
+          btnLink="/"
+        />
+      </div>
+      <div>
+        <div>
+          <h2
+            class="relative max-w-max mx-auto text-3xl uppercase !font-vpc900 md:text-4xl text-center"
+          >
+            Shiftmakers. De kunst
+            <span
+              class="absolute left-0 w-full h-3 -z-10 bottom-2 bg-theme-yellow"
+            ></span>
+          </h2>
+          <h3
+            class="relative max-w-max mx-auto text-3xl uppercase !font-vpc900 2xl:text-4xl text-center"
+          >
+            van (zelf)leiderschap in de 2020’s.
+            <span
+              class="absolute left-0 w-full h-3 -z-10 bottom-2 bg-none sm:bg-theme-yellow"
+            ></span>
+          </h3>
+        </div>
+        <div class="flex flex-col gap-4">
+          <h2 class="my-8 text-3xl font-vp600 md:text-4xl">INHOUD</h2>
+          <h3 class="font-bold uppercase !fontvp-600">
+            Woord vooraf door Wouter Torfs
+          </h3>
+          <ul class="flex flex-col gap-2">
+            <li class="!font-vp400 uppercase">
+              <strong class="!font-vp600">Hoofdstuk 1. </strong>
+              Mijn inspiratie
+            </li>
+            <li class="!font-vp400 uppercase">
+              <strong class="!font-vp600">Hoofdstuk 2. </strong>
+              De focus van ons innerlijke oog
+            </li>
+            <li class="!font-vp400 uppercase">
+              <strong class="!font-vp600">Hoofdstuk 3. </strong>
+              Groei-mindset of statische mindset?
+            </li>
+            <li class="!font-vp400 uppercase">
+              <strong class="!font-vp600">Hoofdstuk 4. </strong>
+              Theory X of Theory Y?
+            </li>
+            <li class="!font-vp400 uppercase">
+              <strong class="!font-vp600">Hoofdstuk 5. </strong>
+              De innerlijke criticus
+            </li>
+            <li class="!font-vp400 uppercase">
+              <strong class="!font-vp600">Hoofdstuk 6. </strong>
+              Talenten en potentieel
+            </li>
+            <li class="!font-vp400 uppercase">
+              <strong class="!font-vp600">Hoofdstuk 7. </strong>
+              Verbinding
+            </li>
+            <li class="!font-vp400 uppercase">
+              <strong class="!font-vp600">Hoofdstuk 8. </strong>
+              Iedereen coach?!
+            </li>
+            <li class="!font-vp400 uppercase">
+              <strong class="!font-vp600">Hoofdstuk 9. </strong>
+              Van individueel naar collectief: bedrijfscultuur
+            </li>
+            <li class="!font-vp400 uppercase">
+              <strong class="!font-vp600">Hoofdstuk 10. </strong>
+              (Zelf)leiderschap
+            </li>
+            <li class="!font-vp400 uppercase">
+              <strong class="!font-vp600">Hoofdstuk 11. </strong>
+              Het perspectief van de coach
+            </li>
+            <li class="!font-vp400 uppercase">
+              <strong class="!font-vp600">Hoofdstuk 12. </strong>
+              Terugblik
+            </li>
+          </ul>
+        </div>
+      </div>
+    </Bounded>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
